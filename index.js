b12ca7f1d88e65e4b0ef64aa09a2fbde20f234c2
erwianaa <erwianaa02@gmail.com>
2024-09-03 19:45:59 +0700

    5 menit

---
+++ index.js
@@ -0,0 +1,17 @@
+// // input        // result
+// [1,2,3,4,5]     // 15
+// [1,2]           // 3
+// [5]             // 5
+// []              // array kosong
+
+var soal1 = [1,2,3,4,5] 
+var soal2 = [1,2]
+var soal3 = [5]
+var soal4 = []
+
+
+function jawabsoal(soal) {
+    for (let i = 0; i < soal.length; i++) {
+
+    }
+}
